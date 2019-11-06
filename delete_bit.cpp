45c6da42af5e850f679e1759582b06ddd541d4d2
TANNU KUMARI <tannuchoudhary10@gmail.com>
2019-11-06 21:40:53 +0530

    Create delete_bit.cpp

---
+++ delete_bit.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+   int num, position;
+   cin>>num;
+   cin>>position;
+   int mask;
+   mask = ~(1<<position);
+   num = (num & mask);
+   cout<<num;
+   return 0;
+   
+}
