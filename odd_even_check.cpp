e23e1ab632c697bda8a215423ae7ecc6c977b3ba
TANNU KUMARI <tannuchoudhary10@gmail.com>
2019-11-06 09:50:54 +0530

    Update odd_even_check.cpp

--- odd_even_check.cpp
+++ odd_even_check.cpp
@@ -1,1 +1,14 @@
-
+#include<iostream>
+using namespace std;
+int main(){
+    int num;
+    cin>>num;
+    int d = (num & (1))!=0?1:0; //if number & 1 is 1 then odd and if number & 1 is 0 then even.
+    if(d==1)
+    cout<<"odd";
+    else
+    cout<<"even";
+    return 0;
+    
+    
+}
