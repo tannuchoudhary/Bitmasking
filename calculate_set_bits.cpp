752d3ab0fd65faa55426bec573b62756ab6290c9
TANNU KUMARI <tannuchoudhary10@gmail.com>
2019-11-06 19:49:11 +0530

    Update calculate_set_bits.cpp

--- calculate_set_bits.cpp
+++ calculate_set_bits.cpp
@@ -11,3 +11,21 @@ int main(){
     cout<<count;
     return 0;
 }
+
+
+/*or you can also apply second method i.e (n)(n-1) rule which is much faster and optimized*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num;
+    cin>>num;
+    int count = 0;
+    while(num){
+        num = (num&(num-1));
+        count++;
+    }
+    cout<<count;
+    return 0;
+}
